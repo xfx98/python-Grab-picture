246e29b6da15a89a2079068b66b4bc38a706bcf8
xfx <48882424+xfx98@users.noreply.github.com>
2019-09-12 10:07:53 +0800

    Update pyselder.py

--- pyselder.py
+++ pyselder.py
@@ -1,32 +1,40 @@
 import os
 import requests
 import threading
-import time
-
 
 def getImg(keyword, localPath, imgCont=50):
     params = []
     for i in range(30, imgCont+90, 30):
         params.append({
             'tn': 'resultjson_com',
-            'ipn' : 'rj',
-            "ie" : "utf-8",
+            'ipn': 'rj',
+            "ie": "utf-8",
             'word': keyword,
             'pn': i
         })
     url = 'https://image.baidu.com/search/acjson'
     urls = []
     for i in params:
-        urls.append(requests.get(url, params=i).json(strict=False).get('data'))
-    if not os.path.exists(localPath):  # 新建文件夹
-        os.mkdir(localPath)
+        while 1:
+            try:
+                urls.append(requests.get(url, params=i).json(strict=False).get('data'))
+                break
+            except:
+                print("连接超时，正在重试...")
+    if not os.path.exists(localPath):
+        os.makedirs(localPath)
     x = 1
     for imgs in urls:
         for img in imgs:
             if img.get('thumbURL'):
                 print('正在下载%s%d：%s' % (keyword, x, img.get('thumbURL')))
-                ir = requests.get(img.get('thumbURL'))
-                f = open(localPath + r'\\' + '%d.jpg' % x, 'wb')
+                while 1:
+                    try:
+                        ir = requests.get(img.get('thumbURL'))
+                        break
+                    except:
+                        print("连接超时，正在重试...")
+                f = open(localPath + r'\\' + '%d.%s' % (x,img.get("type")), 'wb')
                 f.write(ir.content)
                 f.close()
                 x += 1
@@ -35,18 +43,9 @@ def getImg(keyword, localPath, imgCont=50):
             else:
                 print('图片链接不存在')
 
-
 if __name__ == "__main__":
 
-    keywords = set()
-    # while keywords.__len__() < 3:
     keyword = input("输入图片关键字：")
-    if keyword in keywords:
-        print("%s 元素已存在！" % keyword)
-    else:
-        keywords.add(keyword)
-    imgcont = int(input("输入爬取张数："))
-    for cls in keywords:
-        threading.Thread(target=getImg(cls, os.getcwd()+"\\"+cls,imgcont)).start()
+    pnum = int(input("输入爬取图片数量："))
+    threading.Thread(target=getImg(keyword, os.getcwd()+"\\img\\"+keyword, pnum)).start()
     print("下载完成！")
-    os.system("pause")
